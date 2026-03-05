b3567266f1b0a2b7ed88c9ce29ae3e4ba6283508
Omar Joaquim <omarjim492@gmail.com>
2026-03-05 15:46:17 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ self.addEventListener("install", event => {
         "./",
         "./index.html",
         "./manifest.json",
-        "./icon.png"
+        "./icon-512.png"
       ]);
     })
   );
